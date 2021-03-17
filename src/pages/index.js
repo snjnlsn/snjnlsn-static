c640f6e6dcc89eceb0ffc86f8cdd520fefc2f9a1
Sanjay Nelson <services@snjnlsn.co>
2021-03-16 22:52:49 -0400

    breaking kermit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import kermit from "../images/kermit.gif"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="coming soon" />
     <div>
       <div className="about-text">
-        <h1 className="text-block" id="hi">this is a work in progress</h1>
+        <h1 className="text-block" id="hi">this is not my beautiful site</h1>
         <p className="text-block">please come back soon</p>
+        <img src={kermit} alt="Kermit in a Talking Heads spoof" />
       </div>
       <div className="follow-me">
         <a href="https://www.instagram.com/snjnlsn">
